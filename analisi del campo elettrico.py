15073b277345b08339f180d73e9c44258c03ebf6
cristian-caruso <carusoc266@gmail.com>
2025-05-25 17:08:17 +0200

    Add files via upload

--- analisi del campo elettrico.py
+++ analisi del campo elettrico.py
@@ -12,25 +12,29 @@ x = np.linspace(0, L, N)
 e_r = np.where((x<x1) | (x>x2), 1.0, 80)
 
 d = 2 / e_r
-d_sup = -1 / e_r[1:]
-d_inf = -1 / e_r[:-1]
+d_sup = -1 / e_r[:-1]
+d_inf = -1 / e_r[1:]
 
 M = diags([d, d_sup, d_inf], offsets = [0,1,-1]).toarray()
-eg_val, eg_vect = linalg.eigh(M)
+eig_val, eig_vect = linalg.eig(M)
 
+
+eig_vect = eig_vect[:, eig_val.real.argsort()]
+
+
+plt.figure(num="Modi di oscillazione")
 
 
 for i in range(4):
-    plt.plot(x, eg_vect[:,i], label = f"Modo {i+1}")
+    plt.plot(x, eig_vect[:,i], label = f"Modo {i+1}")
 
 
-
-plt.legend()
+plt.grid(True)
 plt.title("Ricostruzione del campo con modi normali")
 plt.xlabel("Posizione x")
 plt.ylabel("Modo normale E")
 plt.axvline(0, color='gray', linewidth=2)
 plt.axvline(L, color='gray', linewidth=2)
 plt.axvspan(x1, x2, color='lightblue', alpha=0.4, label='Dielettrico')
-plt.grid(True)
+plt.legend()
 plt.show()
